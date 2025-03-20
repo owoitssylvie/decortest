4b76ecc0ffe9660b69d19b7a4bb6bbb42f1ff174
owoitssylvie <sylviarose4ef@gmail.com>
2025-03-20 11:41:33 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -69,4 +69,31 @@ document.addEventListener('DOMContentLoaded', () => {
   setInterval(() => {
     spawnConfettiPiece(container);
   }, 1000);
+
+  /********************************
+   * 3) SPARKLY MOUSE TRAIL
+   ********************************/
+  // Creates a cluster of stars on every mousemove
+  window.addEventListener('mousemove', function(e) {
+    // Adjust how many stars + distribution
+    const arr = [1, 0.9, 0.8, 0.5, 0.2];
+
+    arr.forEach(function(i) {
+      const x = (1 - i) * 75;
+      const star = document.createElement('div');
+      star.className = 'star';
+
+      // Random offset around the cursor
+      star.style.top = e.pageY + Math.round(Math.random() * x - x / 2) + 'px';
+      star.style.left = e.pageX + Math.round(Math.random() * x - x / 2) + 'px';
+
+      document.body.appendChild(star);
+
+      // Remove star after random time, so they don't accumulate
+      window.setTimeout(function() {
+        document.body.removeChild(star);
+      }, Math.round(Math.random() * i * 600));
+    });
+  }, false);
+
 });
